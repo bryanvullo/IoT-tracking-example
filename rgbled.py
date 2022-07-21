a47cc4481ff376f84a76719ac34d5240ff300ac2
bryanvullo <65342555+bryanvullo@users.noreply.github.com>
2022-07-21 14:47:36 +0100

    introduced snowplow

--- rgbled.py
+++ rgbled.py
@@ -1,6 +1,18 @@
+# importing snowplow
+import snowplow_tracker
+from snowplow_tracker import Tracker, Emitter
+from snowplow_tracker import SelfDescribingJson
+
+# importing device modules
 from machine import Pin, PWM
 import time
 
+# snowplow initialisation
+e = Emitter('localhost:9090') #some endpoint
+t = Tracker( e,
+            app_id="Bryan's App")
+
+# device initialisation
 redpin = PWM(Pin(13))
 greenpin = PWM(Pin(12))
 bluepin = PWM(Pin(11))
@@ -13,6 +25,15 @@ bluepin.freq(freq_num)
 redbutton = Pin(7, Pin.IN, Pin.PULL_UP)
 yellowbutton = Pin(8, Pin.IN, Pin.PULL_UP)
 greenbutton = Pin(9, Pin.IN, Pin.PULL_UP)
+
+def track_satisfaction(rating):
+    t.track_self_describing_event(
+        # event
+        SelfDescribingJson("iglu:com.myvendor/satisfaction/jsonschema/1-0-0",
+        {
+            "satisfaction_rating": rating
+        }
+    ))
 
 def setRed():
     redpin.duty_u16(0)
@@ -34,48 +55,61 @@ def setOff():
     greenpin.duty_u16(65535)
     bluepin.duty_u16(65535)
 
+def redButtonClick():
+    track_satisfaction("Bad")
+    setRed()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+    setRed()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+    setRed()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+
+def yellowButtonClick():
+    track_satisfaction("Average")
+    setYellow()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+    setYellow()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+    setYellow()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+
+def greenButtonClick():
+    track_satisfaction("Good")
+    setGreen()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+    setGreen()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+    setGreen()
+    time.sleep(0.5)
+    setOff()
+    time.sleep(0.5)
+
+
 while True:
     setOff()
     
     if not redbutton.value():
-        setRed()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
-        setRed()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
-        setRed()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
+        redButtonClick()
         
-    if not yellowbutton.value():
-        setYellow()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
-        setYellow()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
-        setYellow()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
+    elif not yellowbutton.value():
+        yellowButtonClick()
         
-    if not greenbutton.value():
-        setGreen()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
-        setGreen()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
-        setGreen()
-        time.sleep(0.5)
-        setOff()
-        time.sleep(0.5)
+    elif not greenbutton.value():
+        greenButtonClick()
         
