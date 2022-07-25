32a666f86ce65cdb9649861c7639cc05b5121475
bryanvullo <65342555+bryanvullo@users.noreply.github.com>
2022-07-25 14:55:46 +0100

    folder of raspberry pi code

--- raspberry pi/rgbled.py
+++ raspberry pi/rgbled.py
@@ -1,10 +1,8 @@
 # importing device modules
 from machine import Pin, PWM
 import time
-import dht11
 
-    # device initialisation
-# RGB LED 
+# device initialisation
 redpin = PWM(Pin(13))
 greenpin = PWM(Pin(12))
 bluepin = PWM(Pin(11))
@@ -14,13 +12,6 @@ redpin.freq(freq_num)
 greenpin.freq(freq_num)
 bluepin.freq(freq_num)
 
-# temp and humidity sensor
-temperature = 0
-humidity = 0
-dht = dht11.DHT11(15)
-time.sleep(1)
-
-# buttons
 redbutton = Pin(7, Pin.IN, Pin.PULL_UP)
 yellowbutton = Pin(8, Pin.IN, Pin.PULL_UP)
 greenbutton = Pin(9, Pin.IN, Pin.PULL_UP)
@@ -87,28 +78,16 @@ def greenButtonClick():
     setOff()
     time.sleep(0.5)
 
-def sensorRead():
-    # temp and humidity sensor - DHT11 sensor
-    if dht.measure() == 0:
-        print("DHT data error")
-        return
-    temperature = dht.temperature()
-    humidity = dht.humidity()
-    print("temperature: %0.2fC  humidity: %0.2f"%(temperature, humidity) + "%")
-    
 
 while True:
     setOff()
     
     if not redbutton.value():
-        sensorRead()
         redButtonClick()
         
     elif not yellowbutton.value():
-        sensorRead()
         yellowButtonClick()
         
     elif not greenbutton.value():
-        sensorRead()
         greenButtonClick()
         
